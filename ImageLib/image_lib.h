da05dbeae77820843fafd3f8ef73c5b75658566d
ZhengdongHu <462468145@qq.com>
2018-11-21 18:09:41 +0800

    添加

---
+++ ImageLib/image_lib.h
@@ -0,0 +1,20 @@
+/**
+*    author : huzhegndong
+*    e-mail : 462468145@qq.com
+*    date   : 20181121
+*    desc   : test learning
+*    version: 1.0
+*/
+
+#ifndef _STITCH_LIB_H_
+#define _STITCH_LIB_H_
+
+#include "stitch_lib_defs.h"
+
+
+
+
+
+
+#endif // _STITCH_LIB_H_
+
