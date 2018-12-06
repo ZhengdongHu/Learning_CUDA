28183acd957054296997f71f2e22cb4f35e3c361
ZhengdongHu <462468145@qq.com>
2018-12-06 14:45:59 +0800

    添加glut,添加部分代码

--- Learning_CUDA/Learning_CUDA/main.cpp
+++ Learning_CUDA/Learning_CUDA/main.cpp
@@ -7,7 +7,26 @@
 
 using namespace std;
 
-void main()
+typedef enum _TEST_TYPE
 {
+	TEST_NONE = 0,   // 不进行任何测试
+	TEST_MAT_ADD = 1,   // 智能模板测试
+	TEST_SIMPLE_INSERT_SORT = 2,
+
+
+}TEST_TYPE;
+
+void main(int argc, char *argv[])
+{
+
+	TEST_TYPE testType;
+
+	testType = TEST_NONE;
+
+	if (argc > 1){
+		testType = (TEST_TYPE)atoi(argv[1]);
+	}
+
+
 
 }
